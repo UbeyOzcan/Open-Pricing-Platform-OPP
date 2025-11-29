ed930abc2821dc8c0436772730c4da7c094aebca
UbeyOzcan <ozcanubey@outlook.com>
2025-11-29 13:11:19 +0100

    Formating done within the univariate analysis

--- pages/2_Univariate.py
+++ pages/2_Univariate.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.Data import Analyzer
+import pandas as pd
 
 st.title("Univariate Analysis")
 if 'data' not in st.session_state:
@@ -7,7 +8,7 @@ if 'data' not in st.session_state:
 
 else:
     data=st.session_state['data']
-    A =Analyzer(df=data)
+    A = Analyzer(df=data)
     try:
         rfs = A.get_vars(y=st.session_state['model']['Response'], exposure=st.session_state['model']['Offset'])
         rf = st.selectbox('Select Variable',rfs)
@@ -21,9 +22,17 @@ else:
                                   model_name=st.session_state['model']['Model Name'])
 
         uni_df_calc.insert(loc=3, column='Relative Exposure', value=uni_df_calc[st.session_state['model']['Offset']]/uni_df_calc[st.session_state['model']['Offset']].sum())
+        uni_df_calc.insert(loc=1, column=f'{rf}_fmt', value=uni_df_calc[rf])
+        uni_df_calc_edited = st.data_editor(uni_df_calc, disabled=[0, 1, 3, 4, 5])
 
-        st.dataframe(uni_df_calc)
-
+        do_create = st.button('Create')
+        do_delete = st.button('Delete')
+        if do_create:
+            fmt_dict = uni_df_calc_edited.set_index(rf).to_dict()[f'{rf}_fmt']
+            new_col_df = uni_df_calc_edited[[rf, f'{rf}_fmt']]
+            st.session_state['data'] = pd.merge(st.session_state['data'], new_col_df, how='left')
+        if do_delete:
+            st.session_state['data'] = st.session_state['data'].drop(rf, axis=1)
         fig = A.plot_univariate(df=uni_df_calc,
                                 x=rf,
                                 model_name=st.session_state['model']['Model Name'],
